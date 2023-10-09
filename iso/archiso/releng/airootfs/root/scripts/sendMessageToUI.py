503c6fe9e6e39561e6b801ebcfba69d8fa49ac6d
JZITNIK-github <kintiz@seznam.cz>
2023-10-09 12:45:21 +0200

    fix some bugs

--- iso/archiso/releng/airootfs/root/scripts/sendMessageToUI.py
+++ iso/archiso/releng/airootfs/root/scripts/sendMessageToUI.py
@@ -1,6 +1,6 @@
 import sys
 import json
 import os
-data = json.load(open(os.path.join(os.path.expanduser('~'),"/scripts_run.json")))
+data = json.load(open(os.path.join(os.path.expanduser('~'),"scripts_run.json")))
 data.append(sys.argv[1])
-json.dump(data, open(os.path.join(os.path.expanduser('~'),"/scripts_run.json"), "w"))
+json.dump(data, open(os.path.join(os.path.expanduser('~'),"scripts_run.json"), "w"))
--- iso/install-scripts/scripts/media.sh
+++ iso/install-scripts/scripts/media.sh
@@ -3,27 +3,27 @@ case $1 in
 up)
 	pamixer -u
 	pamixer -i 5 --allow-boost
-	python sendMessageToUI.py "AudioEditor.popup.volumeUp()"
+	python ~/scripts/sendMessageToUI.py "AudioEditor.popup.volumeUp()"
 	;;
 down)
 	pamixer -u
 	pamixer -d 5 --allow-boost
-	python sendMessageToUI.py "AudioEditor.popup.volumeDown()"
+	python ~/scripts/sendMessageToUI.py "AudioEditor.popup.volumeDown()"
 	;;
 mute)
 	pamixer -t
-    python sendMessageToUI.py "AudioEditor.popup.volumeMute()"
+    python ~/scripts/sendMessageToUI.py "AudioEditor.popup.volumeMute()"
 	;;
 play-pause)
 	playerctl play-pause
-    python sendMessageToUI.py "AudioEditor.popup.pause()"
+    python ~/scripts/sendMessageToUI.py "AudioEditor.popup.pause()"
 	;;
 previous)
 	playerctl previous
-    python sendMessageToUI.py "AudioEditor.popup.previous()"
+    python ~/scripts/sendMessageToUI.py "AudioEditor.popup.previous()"
 	;;
 next)
 	playerctl next
-    python sendMessageToUI.py "AudioEditor.popup.next()"
+    python ~/scripts/sendMessageToUI.py "AudioEditor.popup.next()"
 	;;
 esac
