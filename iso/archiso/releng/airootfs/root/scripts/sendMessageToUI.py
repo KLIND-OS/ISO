42f42042f4cc00ac74f3d9f44649b55362d393ac
JZITNIK-github <kintiz@seznam.cz>
2023-10-09 11:19:40 +0200

    fix bug

--- iso/archiso/releng/airootfs/root/scripts/sendMessageToUI.py
+++ iso/archiso/releng/airootfs/root/scripts/sendMessageToUI.py
@@ -1,5 +1,6 @@
 import sys
 import json
-data = json.load(open("/home/kuba/scripts_run.json"))
+import os
+data = json.load(open(os.path.join(os.path.expanduser('~'),"/scripts_run.json")))
 data.append(sys.argv[1])
-json.dump(data, open("/home/kuba/scripts_run.json", "w"))
+json.dump(data, open(os.path.join(os.path.expanduser('~'),"/scripts_run.json"), "w"))
--- iso/install-scripts/scripts/sendMessageToUI.py
+++ iso/install-scripts/scripts/sendMessageToUI.py
@@ -1,5 +1,6 @@
 import sys
 import json
-data = json.load(open("/home/kuba/scripts_run.json"))
+import os
+data = json.load(os.path.join(os.path.expanduser('~'),"/scripts_run.json"))
 data.append(sys.argv[1])
-json.dump(data, open("/home/kuba/scripts_run.json", "w"))
+json.dump(data, os.path.join(os.path.expanduser('~'),"/scripts_run.json"), "w"))
