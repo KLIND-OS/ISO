c79714ec0119b15d09a81cb8bdb93caef28b85e6
JZITNIK-github <kintiz@seznam.cz>
2023-10-09 11:20:39 +0200

    fix bug

--- iso/install-scripts/scripts/sendMessageToUI.py
+++ iso/install-scripts/scripts/sendMessageToUI.py
@@ -3,4 +3,4 @@ import json
 import os
 data = json.load(os.path.join(os.path.expanduser('~'),"/scripts_run.json"))
 data.append(sys.argv[1])
-json.dump(data, os.path.join(os.path.expanduser('~'),"/scripts_run.json"), "w"))
+json.dump(data, open(os.path.join(os.path.expanduser('~'),"/scripts_run.json"), "w"))
