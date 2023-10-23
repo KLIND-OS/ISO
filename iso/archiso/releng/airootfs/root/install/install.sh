0e4c7022c269552205e207eaa907d33e0236320d
JZITNIK-github <kintiz@seznam.cz>
2023-10-23 11:54:41 +0200

    add support for bluetooth

--- iso/archiso/releng/airootfs/root/install/install.sh
+++ iso/archiso/releng/airootfs/root/install/install.sh
@@ -138,9 +138,10 @@ pacman -S --noconfirm grub
 grub-install --target=i386-pc "$selected_disk"  
 cp /etc/default/grub /etc/default/grub.backup
 
-pacman -S --noconfirm $DRI nano git networkmanager xorg xorg-xinit picom alacritty chromium base-devel xmonad xmonad-contrib nodejs dialog npm fuse2 pipewire pipewire-pulse pavucontrol dunst libnotify nm-connection-editor rofi inotify-tools gparted pamixer playerctl cups
+pacman -S --noconfirm $DRI nano git networkmanager xorg xorg-xinit picom alacritty chromium base-devel xmonad xmonad-contrib nodejs dialog npm fuse2 pipewire pipewire-pulse pavucontrol dunst libnotify nm-connection-editor rofi inotify-tools gparted pamixer playerctl cups bluez bluez-utils blueman
 systemctl enable NetworkManager
 systemctl enable cups
+systemctl enable bluetooth
 
 touch ~/.xinitrc
 echo "exec /usr/bin/pipewire &" >> ~/.xinitrc
