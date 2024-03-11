37492af35e6ca53e79af84e7699f0ecf8778dd90
JZITNIK-github <email@jzitnik.is-a.dev>
2024-03-11 07:55:26 +0100

    Add package to install and disable mouse in xmonad

--- iso/archiso/releng/airootfs/root/config/xmonad.hs
+++ iso/archiso/releng/airootfs/root/config/xmonad.hs
@@ -10,7 +10,6 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.SpawnOnce ( spawnOnce )
 import Graphics.X11.ExtraTypes.XF86 (xF86XK_AudioLowerVolume, xF86XK_AudioRaiseVolume, xF86XK_AudioMute, xF86XK_MonBrightnessDown, xF86XK_MonBrightnessUp, xF86XK_AudioPlay, xF86XK_AudioPrev, xF86XK_AudioNext)
 
-
 startup :: X ()
 startup = do
     spawn "xsetroot -cursor_name left_ptr"
@@ -20,8 +19,14 @@ startup = do
     spawnOnce "picom"
     spawnOnce "bash /root/startUI.sh"
 
+myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
+    [ ((modm, button1), (\_ -> return ()))
+    , ((modm, button2), (\w -> focus w >> windows W.swapMaster))
+    , ((modm, button3), (\_ -> return ()))
+    ]
+
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
-    [ 
+    [
     ((modm .|. shiftMask, xK_c     ), kill)
 
     , ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
@@ -35,7 +40,6 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((0,                    xF86XK_AudioMute), spawn "bash /root/scripts/media.sh mute")
     ]
 
-
 main = xmonad $ desktopConfig
     { terminal           = "alacritty"
     , layoutHook         = smartBorders $ Full
@@ -45,5 +49,7 @@ main = xmonad $ desktopConfig
     , focusedBorderColor = "#000000"
     , startupHook        = startup
     , keys               = myKeys
+    , mouseBindings      = myMouseBindings
     , modMask            = mod4Mask
     }
+
