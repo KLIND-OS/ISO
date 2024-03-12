12ae3d1af6e9afcdb51d902ffba95491085cd7d8
JZITNIK-github <email@jzitnik.is-a.dev>
2024-03-12 21:12:35 +0100

    Some changes

--- iso/archiso/makeiso.sh
+++ iso/archiso/makeiso.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 mkarchiso -v -w ./output -o ./output releng/
+mkdir final
+cp ./output/klindos*.iso ./final/KLINDOS.iso
+rm -rf ./output
+sha256sum ./final/KLINDOS.iso > ./final/KLINDOS.iso.sha256
